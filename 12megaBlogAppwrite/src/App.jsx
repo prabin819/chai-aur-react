1db607ee118eaab36632cc8fcbcb4978d812912e
Prabin <manandharprabin819@gmail.com>
2024-06-23 18:04:52 +0545

    blogwithappwrite env variables and authenticaton service done

---
+++ 12megaBlogAppwrite/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {console.log(import.meta.env.VITE_APPWRITE_URL)}
+      <h1>Blog with appwrite</h1>
+    </>
+  )
+}
+
+export default App
