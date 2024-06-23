6570e7391ee8f3f6ec83634524eb6a01298168fd
Prabin <manandharprabin819@gmail.com>
2024-06-24 02:27:38 +0545

    file services done

--- 12megaBlogAppwrite/src/appwrite/config.js
+++ 12megaBlogAppwrite/src/appwrite/config.js
@@ -67,9 +67,34 @@ export class Service{
             return await this.databases.listDocuments(conf.appwriteDatabaseId, conf.appwriteCollectionId, queries)
             
         } catch (error) {
-            console.log("Appwrite service :: getPost :: error ", error);
+            console.log("Appwrite service :: getPosts :: error ", error);
             return false;
         }
+    }
+
+    //file upload service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(conf.appwriteBucketId, ID.unique(),file)
+        } catch (error) {
+            console.log("Appwrite service :: fileupload :: error ", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(conf.appwriteBucketId,fileId)
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error ", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(conf.appwriteBucketId,fileId)
     }
 }
 
